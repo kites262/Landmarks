523ae9ad77633ff8d1ef48166cfb86cdbf3640d9
kites262 <47177568+kites262@users.noreply.github.com>
2024-01-28 13:19:34 +0800

    fix unexpected permission checking message

--- src/main/java/us/xuanxi/landmarks/utils/PermissionChecker.java
+++ src/main/java/us/xuanxi/landmarks/utils/PermissionChecker.java
@@ -4,16 +4,19 @@ import org.bukkit.command.CommandSender;
 import us.xuanxi.landmarks.data.Finals;
 
 public class PermissionChecker {
+    public static boolean infoHas(CommandSender sender, String permission){
+        if(without(sender, permission)){
+            sender.sendMessage(Finals.msg_no_permission);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public static boolean without(CommandSender sender, String permission){
         if(sender.hasPermission(Finals.permission_admin)){
             return false;
         }
-
-        if(sender.hasPermission(permission)){
-            return false;
-        }else{
-            sender.sendMessage(Finals.msg_no_permission);
-            return true;
-        }
+        return !sender.hasPermission(permission);
     }
 }
